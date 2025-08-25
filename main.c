2cebb59bdbd8ed5bae3324a9097c0a033253f233
TP89k <timoshapt@mail.ru>
2025-08-25 15:04:47 +0300

    splited on files

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <TXLib.h>
+
+#include "equation_data.h"
+#include "equation_tests.h"
+
+int main() {
+    Equation_data equation_data;
+
+    testing_program();
+
+    equation_data = input();
+
+    equation_data = solve_quadratic_equation(equation_data);
+    print_results(equation_data);
+
+    return 0;
+}
